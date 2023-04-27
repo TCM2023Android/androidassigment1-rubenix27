f046aa641cbc3d6e300e57bd4c1aa7306fd234bd
carlosguinovart <103029861+carlosguinovart@users.noreply.github.com>
2023-04-27 11:53:27 +0200

    cotxeClass-NotEnded

--- Practica1/app/src/main/java/com/example/myapplication/Cotxe.java
+++ Practica1/app/src/main/java/com/example/myapplication/Cotxe.java
@@ -1,8 +1,14 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Cotxe {
+import java.util.Random;
+
+import androidx.annotation.NonNull;
+
+public class Cotxe implements Parcelable{
 
    private String alias;
    private String marca;
@@ -13,6 +19,42 @@ public class Cotxe {
    private double longitud;
    private double latitud;
 
+   public Cotxe(String alias, String marca, String model, double cilindrada, long telefon, boolean automatic, double longitud, double latitud){
+      this.alias=alias;
+      this.marca=marca;
+      this.model=model;
+      this.cilindrada=cilindrada;
+      this.telefon=telefon;
+      this.automatic=automatic;
+      this.longitud=longitud;
+      this.latitud=latitud;
 
+   }
+   public Cotxe(Parcel in) {
+      super();
+      readFromParcel(in);
 
+   }
+
+   public static final Parcelable.Creator<Cotxe> CREATOR = new Parcelable.Creator<Cotxe>() {
+      @Override
+      public Cotxe createFromParcel(Parcel parcel) {
+         return null;
+      }
+
+      @Override
+      public Cotxe[] newArray(int i) {
+         return new Cotxe[0];
+      }
+   };
+
+   @Override
+   public int describeContents() {
+      return 0;
+   }
+
+   @Override
+   public void writeToParcel(@NonNull Parcel parcel, int i) {
+
+   }
 }
