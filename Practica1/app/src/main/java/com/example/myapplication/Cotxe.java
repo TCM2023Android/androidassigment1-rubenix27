fa5f0a6271a3807d2290ea527187203fe2624d0b
rubenix27 <98050873+rubenix27@users.noreply.github.com>
2023-04-27 11:30:12 +0200

    commit1

---
+++ Practica1/app/src/main/java/com/example/myapplication/Cotxe.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+public class Cotxe {
+
+   private String alias;
+   private String marca;
+   private String model;
+   private double cilindrada;
+   private long telefon;
+   private boolean automatic;
+   private double longitud;
+   private double latitud;
+
+
+
+}
--- Practica1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Practica1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.action_add:
+                Log.v("Rubén","menu clicked");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
