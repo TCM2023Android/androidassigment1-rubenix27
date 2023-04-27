3f8376788ed63c3f4931d054dd8b33f51dcc29bd
carlosguinovart <103029861+carlosguinovart@users.noreply.github.com>
2023-04-27 11:55:13 +0200

    Update Cotxe.java

--- Practica1/app/src/main/java/com/example/myapplication/Cotxe.java
+++ Practica1/app/src/main/java/com/example/myapplication/Cotxe.java
@@ -36,6 +36,70 @@ public class Cotxe implements Parcelable{
 
    }
 
+   public String getAlias() {
+      return alias;
+   }
+
+   public void setAlias(String alias) {
+      this.alias = alias;
+   }
+
+   public String getMarca() {
+      return marca;
+   }
+
+   public void setMarca(String marca) {
+      this.marca = marca;
+   }
+
+   public String getModel() {
+      return model;
+   }
+
+   public void setModel(String model) {
+      this.model = model;
+   }
+
+   public double getCilindrada() {
+      return cilindrada;
+   }
+
+   public void setCilindrada(double cilindrada) {
+      this.cilindrada = cilindrada;
+   }
+
+   public long getTelefon() {
+      return telefon;
+   }
+
+   public void setTelefon(long telefon) {
+      this.telefon = telefon;
+   }
+
+   public boolean isAutomatic() {
+      return automatic;
+   }
+
+   public void setAutomatic(boolean automatic) {
+      this.automatic = automatic;
+   }
+
+   public double getLongitud() {
+      return longitud;
+   }
+
+   public void setLongitud(double longitud) {
+      this.longitud = longitud;
+   }
+
+   public double getLatitud() {
+      return latitud;
+   }
+
+   public void setLatitud(double latitud) {
+      this.latitud = latitud;
+   }
+
    public static final Parcelable.Creator<Cotxe> CREATOR = new Parcelable.Creator<Cotxe>() {
       @Override
       public Cotxe createFromParcel(Parcel parcel) {
