e11f54f6b33787c51f8afa804546ef32c58435e8
rubenix27 <98050873+rubenix27@users.noreply.github.com>
2023-05-04 11:41:37 +0200

    km

--- Practica1/app/src/main/java/com/example/myapplication/AfegirCotxe.java
+++ Practica1/app/src/main/java/com/example/myapplication/AfegirCotxe.java
@@ -1,16 +1,26 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AfegirCotxe extends AppCompatActivity {
+public class AfegirCotxe extends AppCompatActivity{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_cotxe);
+    }
+
+    public void recollirDadesCotxe(View view){
+        String text = editText.getText().toString();
+        Intent resultat = new Intent();
+        resultat.putExtra("text_resultat",text);
+        setResult(RESULT_OK,resultat);
+        finish(); //tanca la pantalla
     }
 
 
