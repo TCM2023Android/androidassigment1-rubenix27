bd29ddc5ff97b613f41836e60335d3ae66944cde
carlosguinovart <103029861+carlosguinovart@users.noreply.github.com>
2023-05-07 19:01:35 +0200

    update 2

--- Practica1/app/src/main/java/com/example/myapplication/AfegirCotxe.java
+++ Practica1/app/src/main/java/com/example/myapplication/AfegirCotxe.java
@@ -10,20 +10,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AfegirCotxe extends AppCompatActivity{
 
-    EditText editText;
+    EditText etAlias, etMarca, etModel, etCilindrada, etTelefon, etLongitud, etLatitud;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_cotxe);
+
+        etAlias= findViewById(R.id.etAlias);
+        etMarca= findViewById(R.id.etMarca);
+        etModel= findViewById(R.id.etModel);
+        etCilindrada= findViewById(R.id.etCilindrada);
+        etTelefon= findViewById(R.id.etTelefon);
+        etLongitud= findViewById(R.id.etLongitud);
+        etLatitud= findViewById(R.id.etLatitud);
+
+
+
     }
 
     public void recollirDadesCotxe(View view){
-        String text = editText.getText().toString();
+        String alias = etAlias.getText().toString();
+        String marca = etMarca.getText().toString();
+        String model = etModel.getText().toString();
+        String cilindrada =etCilindrada.getText().toString();
+        String telefon =etTelefon.getText().toString();
+
+
+        String latitud = etLatitud.getText().toString();
+
+
         Intent resultat = new Intent();
-        resultat.putExtra("text_resultat",text);
+        //mirar
+        resultat.putExtra("text_resultat", (CharSequence) etAlias);
         setResult(RESULT_OK,resultat);
         finish(); //tanca la pantalla
+    }
+
+    public void cancelBtn(View view){
+
+        finish();
     }
 
 
