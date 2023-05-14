f5a46da2b6880fafa7780f6ca8a7f895c3f5a544
carlosguinovart <103029861+carlosguinovart@users.noreply.github.com>
2023-05-14 19:43:14 +0200

    cotxeInfo

--- Practica1/app/src/main/java/com/example/myapplication/infoCotxe.java
+++ Practica1/app/src/main/java/com/example/myapplication/infoCotxe.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class infoCotxe extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class infoCotxe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_cotxe);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_datail_cotxe, menu);
+        return true;
     }
 }
